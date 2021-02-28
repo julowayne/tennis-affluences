fdad60a26f0e860289c47199194e69e54cb709af
Jules Thomas D <48414478+julowayne@users.noreply.github.com>
2021-02-28 13:40:57 +0100

    changed controllers name to fix class does not exist in prod

--- routes/web.php
+++ routes/web.php
@@ -5,11 +5,11 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [\App\Http\Controllers\IndexController::class, 'index'])->name('welcome');
 
-Route::get('/reservation', [\App\Http\Controllers\reservationController::class, 'reservation'])->name('reservation');
+Route::get('/reservation', [\App\Http\Controllers\ReservationController::class, 'reservation'])->name('reservation');
 
-Route::post('/reservation', [\App\Http\Controllers\reservationController::class, 'confirmReservation'])->name('reservation.reservation');
+Route::post('/reservation', [\App\Http\Controllers\ReservationController::class, 'confirmReservation'])->name('reservation.reservation');
 
-Route::get('/reservation/annulation/{token}', [\App\Http\Controllers\cancelController::class, 'cancelView'])->name('cancel.reservation');
+Route::get('/reservation/annulation/{token}', [\App\Http\Controllers\CancelController::class, 'cancelView'])->name('cancel.reservation');
 
-Route::post('/reservation/annulation/{token}', [\App\Http\Controllers\cancelController::class, 'cancel'])->name('cancel.confirm');
+Route::post('/reservation/annulation/{token}', [\App\Http\Controllers\CancelController::class, 'cancel'])->name('cancel.confirm');
 
